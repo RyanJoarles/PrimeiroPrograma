92216da402a128fd8a549e79bd433bea01606cb5
Ryan Joarles <ryanjoarles@gmail.com>
2022-04-09 20:55:15 -0300

    Primeiros códigos em Java

---
+++ src/main/java/com/mycompany/primeiroprograma/PrimeiroPrograma.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.primeiroprograma;
+
+/**
+ *
+ * @author hawoc
+ */
+
+public class PrimeiroPrograma {
+    /*  Aula 04 Curso em Vídeo
+    psvm
+    
+    CamelCase:
+        MinhaClasse = AlunosCursoEmVideo(É uma classe)
+        meuAtributo = nomeAluno(É um atributo)
+        minhaVariavel = mediaPrimeiroBimestre(É uma variável)
+        meuMétodo = lancarNotaAluno(É um método)
+        meu_pacote = alunoscursoemvideo(Nome do pacote)
+        MINHA_CONSTANTE = VALOR_DE_PI(É uma constante)
+    */
+    
+
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+        /*sout*/
+        System.out.println("\nTestando sout e tab.");       
+        
+        /*println serve para printar e pular para a linha de baixo*/
+    }
+}
